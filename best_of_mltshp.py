af126e67ba9d6662d26b85d5a3314c2b6dfdfe13
Dan Phiffer <dan@phiffer.org>
2024-05-20 09:16:41 -0400

    use requests to load feeds

--- best_of_mltshp.py
+++ best_of_mltshp.py
@@ -15,6 +15,10 @@ MASTODON_TOKEN = os.environ["MASTODON_TOKEN"]
 
 IDEMPOTENCY_KEY = "".join(random.choices(string.ascii_lowercase + string.digits, k=12))
 
+def load_feed(url):
+    response = requests.get(url)
+    return feedparser.parse(response.text)
+
 def encode_toot(feed_entry):
     return f"{feed_entry.link} “{feed_entry.title}”"
 
@@ -36,17 +40,15 @@ def post_toot(toot):
         print(f"Failed to post {toot}")
         print(response.status_code)
 
-# Don't verify SSL certs
-if hasattr(ssl, '_create_unverified_context'):
-    ssl._create_default_https_context = ssl._create_unverified_context
-
 print("Loading MLTSHP Popular RSS feed")
-input_feed = feedparser.parse("https://mltshp.com/user/mltshp/rss")
+input_feed = load_feed("https://mltshp.com/user/mltshp/rss")
+if len(input_feed.entries) == 0:
+    raise Exception("No popular posts found, something went wrong")
 input_feed.entries.reverse() # Start with the oldest entry in the popular feed
 print(f"  found {len(input_feed.entries)} entries")
 
 print(f"Loading {MASTODON_USER} RSS feed")
-output_feed = feedparser.parse(f"https://{MASTODON_INSTANCE}/{MASTODON_USER}.rss?limit=200")
+output_feed = load_feed(f"https://{MASTODON_INSTANCE}/{MASTODON_USER}.rss?limit=200")
 print(f"  found {len(output_feed.entries)} entries")
 
 already_tooted = []
